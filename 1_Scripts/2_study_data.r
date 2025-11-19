6db3eff5960bd8ee482b5c608b46f8ee6ba41fc8
Turjya Laskar <turjyalaskar@Turjyas-MacBook-Air.local>
2025-11-19 18:38:11 +0530

    Changes: Rename and cooding

---
+++ 1_Scripts/2_study_data.r
@@ -0,0 +1,8 @@
+
+# Sourcing Raw Data
+source("/Users/turjyalaskar/Documents/R_Project/SEM/SEMRepo/1_Scripts/raw_data_loading.r")
+
+# Libraries
+
+study_nigeria <- nigeria_raw_data %>% 
+  select()
